9f95ce025a0ffe69b2c2d56893cec4d306179949
RobbTom <kacper.r.tomczynski@gmail.com>
2021-02-02 00:45:20 +0100

    Added URL handling

---
+++ youtubeios/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  youtubeios
+//
+//  Created by user on 01/02/2021.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
