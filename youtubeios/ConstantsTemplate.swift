8e6ce7f515eac502c217ab1792b7925894b068ce
RobbTom <kacper.r.tomczynski@gmail.com>
2021-02-02 01:08:33 +0100

    modified gitignore to stop tracking Constants.swift, added ConstantsTemplates.swift instead

---
+++ youtubeios/ConstantsTemplate.swift
@@ -0,0 +1,22 @@
+//
+//  ConstantsTemplate.swift
+//  youtubeios
+//
+//  Created by user on 02/02/2021.
+//
+
+
+
+//Add your own key below
+
+import Foundation
+/*
+struct Constants{
+
+    static var API_KEY =""
+    static var PLAYLIST_ID = ""
+    static var API_URL = ""
+
+}
+ */
+       
